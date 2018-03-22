71714430175cc743413913341ee5e5eef82b0da7
Natalbornoz <natalbornoz@gmail.com>
2018-03-22 12:41:27 -0300

    create product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddProduct from './AddProduct';
 import ProductItem from './ProductItem';
 
 const products = [
@@ -25,7 +26,8 @@ class App extends Component {
         products: JSON.parse(localStorage.getItem('products'))
       };
 
-      this.onDelete = this.onDelete = this.onDelete.bind(this);
+      this.onAdd = this.onAdd.bind(this);
+      this.onDelete = this.onDelete.bind(this);
 
     }
   
@@ -38,6 +40,19 @@ class App extends Component {
   //Metodo para obtener productos
   getProducts() {
     return this.state.products;
+  }
+
+  //Metodo para agregar elementos min 44:24 / 49:33
+  onAdd(name, price) {
+    const products = this.getProducts ();
+
+    products.push({
+      name,
+      price
+    });
+
+    this.setState ({ products });
+    // console.log(name, price);
   }
 
   //Tomara un nombre como parametro
@@ -55,6 +70,11 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Products Manager</h1>
+
+       <AddProduct
+        onAdd= {this.onAdd}
+       />
+
       {
         this.state.products.map(product => {
           return (
